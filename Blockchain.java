d4429c1a9b69bdcc523d9da1c384a598a3c08e86
Lan Yang <lanyang0430.us@gmail.com>
2018-06-20 23:59:40 -0400

    Update Blockchain.java

--- Blockchain.java
+++ Blockchain.java
@@ -8,14 +8,14 @@ public class Blockchain {
 	public static void main(String[] args) {
 		blockchain.add(new Block("First Block", "0") );
 		System.out.println("Trying to Mine block 1...");
-		blockchain.get(0).mineBlock(difficulty);
+		blockchain.get(0).mining(difficulty);
 		
 		blockchain.add(new Block("Second Block", blockchain.get(blockchain.size()-1).hash) );
 		System.out.println("Trying to Mine block 2...");
-		blockchain.get(1).mineBlock(difficulty);
+		blockchain.get(1).mining(difficulty);
 		blockchain.add(new Block("Third Block", blockchain.get(blockchain.size()-1).hash) );
 		System.out.println("Trying to Mine block 3...");
-		blockchain.get(2).mineBlock(difficulty);
+		blockchain.get(2).mining(difficulty);
 		
 		System.out.println("\nBlockchain is Valid: " + isChainValid());
 		
@@ -24,23 +24,14 @@ public class Blockchain {
 		System.out.println("\nThe block chain: ");
 		System.out.println(blockchainJson);
 
-		
-		/*Block genesisBlock = new Block("First Block", "0");
-		System.out.println("Hash for block 1: "+genesisBlock.hash);
-		
-		Block secondBlock = new Block("Second Block", genesisBlock.hash);
-		System.out.println("Hash for block 2: "+secondBlock.hash);
-		
-		Block thirdBlock = new Block("Third Block", secondBlock.hash);
-		System.out.println("Hash for block 3: "+secondBlock.hash);
-		*/
-
+	
 	}
 	public static Boolean isChainValid() {
 		Block currentBlock;
 		Block previousBlock;
 		String hashTarget = new String(new char[difficulty]).replace('\0', '0');
-		for(int i =1;i<blockchain.size();i++) {
+		for(int i =1;i<blockchain.size();i++) 
+		{
 			currentBlock=blockchain.get(i);
 			previousBlock=blockchain.get(i-1);
 			if(!currentBlock.hash.equals(currentBlock.calculateHash())) {
